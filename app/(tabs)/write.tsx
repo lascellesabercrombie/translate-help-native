7b8c523dc00ca8e4406703661f5e8d6d5ba588ec
Alex Grafen <alex.grafen@gmail.com>
2024-10-17 17:12:50 +0100

    fix: nesting FlatList inside ScrollView

--- app/(tabs)/write.tsx
+++ app/(tabs)/write.tsx
@@ -27,6 +27,7 @@ export default function Write() {
     console.log('change')
   }
     return (
+      <>
       <ParallaxScrollView
         headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
         headerImage={<Ionicons size={310} name="pencil" style={styles.headerImage} />}>
@@ -35,25 +36,26 @@ export default function Write() {
         </ThemedView>
         {texts ? <Pressable onPress={handlePress}>
           <Text>{selectedText?.original.title}</Text>
-          <Modal visible={dropdownVisible} transparent={true} animationType="fade">
-            <View style={styles.modalContainer}>
-              <FlatList
-                data={texts}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                  <Pressable onPress={() => handleSelect(item)} style={styles.dropdownItem}>
-                    <ThemedText>{item.original.title}</ThemedText>
-                  </Pressable>
-                )}
-              />
-            </View>
-          </Modal>
         </Pressable> : null}
         <TextInput
         multiline={true}
         onChangeText={handleChange}>
         </TextInput>
       </ParallaxScrollView>
+         <Modal visible={dropdownVisible} transparent={true} animationType="fade">
+         <View style={styles.modalContainer}>
+           <FlatList
+             data={texts}
+             keyExtractor={(item) => item.id.toString()}
+             renderItem={({ item }) => (
+               <Pressable onPress={() => handleSelect(item)} style={styles.dropdownItem}>
+                 <ThemedText>{item.original.title}</ThemedText>
+               </Pressable>
+             )}
+           />
+         </View>
+       </Modal>
+       </>
     );
 }
 
