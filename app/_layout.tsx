2136caa26a6e0c70b28e85586520ba73536b6c90
Alex Grafen <alex.grafen@gmail.com>
2024-10-24 16:06:33 +0100

    feat: replace context provider with database provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,84 +2,17 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { createContext, useEffect, useState, Dispatch, SetStateAction } from 'react';
+import { useEffect, Suspense } from 'react';
 import 'react-native-reanimated';
-
+import { SQLiteProvider} from 'expo-sqlite';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { migrateDbIfNeeded } from '../assets/databases/db';
+import { Fallback } from './fallback';
 
-export interface TextType {
-  id: number;
-  original: {
-  title: string;
-  authorFamilyName: string;
-  authorPersonalName?: string;
-  text: string;
-  };
-  translation: {
-    title?: string;
-    authorFamilyName?: string;
-    authorPersonalName?: string;
-    text?: string;
-  }
-};
-
-interface TextContextType {
-  texts: TextType[];
-  setTexts: Dispatch<SetStateAction<TextType[]>>;
-};
-export const TextContext = createContext<TextContextType | null>(null);
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  
-  const [texts, setTexts] = useState<TextType[]>([
-    {
-      id: 1,
-      original: {
-        title: "静夜思",
-        authorFamilyName: "李",
-        authorPersonalName: "白",
-        text: `床前明月光，\n疑是地上霜。\n举头望明月，\n低头思故乡。`,
-      },
-      translation: {
-        title: "Quiet Night Thoughts",
-        authorFamilyName: "Li",
-        authorPersonalName: "Bai",
-        text: "",
-      },
-    },
-    {
-      id: 2,
-      original: {
-        title: "悯农",
-        authorFamilyName: "李",
-        authorPersonalName: "绅",
-        text: `锄禾日当午，\n汗滴禾下土。\n谁知盘中餐，\n粒粒皆辛苦。`,
-      },
-      translation: {
-        title: "Peasants",
-        authorFamilyName: "Li",
-        authorPersonalName: "Shen",
-        text: "",
-      },
-    },
-    {
-      id: 3,
-      original: {
-        title: "咏鹅",
-        authorFamilyName: "骆",
-        authorPersonalName: "宾王",
-        text: `鹅、鹅、鹅，\n曲项向天歌。\n白毛浮绿水，\n红掌拨清波`,
-      },
-      translation: {
-        title: "To the Goose",
-        authorFamilyName: "Luo",
-        authorPersonalName: "Binwang",
-        text: "",
-      },
-    },
-  ]);
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -96,13 +29,15 @@ export default function RootLayout() {
   }
 
   return (
-    <TextContext.Provider value={{texts, setTexts}}>
+    <Suspense fallback={<Fallback />}>
+    <SQLiteProvider databaseName="test.db" onInit={migrateDbIfNeeded} useSuspense>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
-    </TextContext.Provider>
+    </SQLiteProvider>
+    </Suspense>
   );
 }
