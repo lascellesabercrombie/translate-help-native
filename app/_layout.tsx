861ba6e4aa205c9185a185150f56f35100a69e04
Alex Grafen <alex.grafen@gmail.com>
2024-10-29 09:36:27 +0000

    feat: reintroduce context, but just for text id

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, Suspense } from 'react';
+import { createContext, useEffect, useState, Suspense, SetStateAction, Dispatch } from 'react';
 import 'react-native-reanimated';
 import { SQLiteProvider} from 'expo-sqlite';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -12,7 +12,14 @@ import { Fallback } from './fallback';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+interface TextContextType {
+  selectedTextId: number | null;
+  setSelectedTextId: Dispatch<SetStateAction<number | null>>;
+};
+export const TextContext = createContext<TextContextType | null>(null);
+
 export default function RootLayout() {
+  const [selectedTextId, setSelectedTextId] = useState<number | null>(null);
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -31,12 +38,14 @@ export default function RootLayout() {
   return (
     <Suspense fallback={<Fallback />}>
     <SQLiteProvider databaseName="test.db" onInit={migrateDbIfNeeded} useSuspense>
+    <TextContext.Provider value={{ selectedTextId, setSelectedTextId}}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
+    </TextContext.Provider>
     </SQLiteProvider>
     </Suspense>
   );
