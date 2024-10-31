4cfdf0b6effee6537e1ee032b6d656e9bd77afe5
Alex Grafen <alex.grafen@gmail.com>
2024-10-31 10:18:03 +0000

    feat: update Write component to work with database and save text

--- app/(tabs)/write.tsx
+++ app/(tabs)/write.tsx
@@ -3,29 +3,68 @@ import { Pressable, StyleSheet, TextInput } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { useContext, useState } from 'react';
-import { View, Text, Modal, FlatList } from 'react-native';
-import { TextContext, TextType } from '@/app/_layout'
-
+import { useContext, useEffect, useState } from 'react';
+import { View, Modal } from 'react-native';
+import { TextContext } from '@/app/_layout';
+import { useSQLiteContext } from 'expo-sqlite';
+import { Text as TextType } from '@/types/data';
 
 export default function Write() {
+  const db = useSQLiteContext();
   const textContext = useContext(TextContext);
-  const texts = textContext?.texts;
+  const selectedTextId = textContext?.selectedTextId;
+  const [textObject, setTextObject] = useState<Partial<TextType> | null>(null);
+  const [text, setText] = useState('');
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  const [selectedText, setSelectedText] = useState(texts?.[0]);
-
   function handlePress () {
     setDropdownVisible(!dropdownVisible);
   };
 
-  function handleSelect (text: TextType) {
-    setSelectedText(text);
-    setDropdownVisible(false);
+  useEffect(() => {
+    let mounted = true;
+    
+    const fetchData = async () => {
+      if (!selectedTextId) {
+        // console.error('No id found');
+        return;
+      }
+      const allRows: TextType | null = await db.getFirstAsync(
+          'SELECT * FROM texts WHERE texts.original_text_id = ?;', [selectedTextId]);
+        if (allRows) {
+          setTextObject(allRows);
+          setText(allRows.text);
+      } 
+    };
+    
+    fetchData();
+
+    return () => {
+      mounted = false;
+    };
+  }, [selectedTextId]);
+
+  function handlePressSaveButton () {
+    if (selectedTextId === undefined || selectedTextId === null) {
+      throw new Error('No original text id found');
+    }
+    setTextObject(prevState => {
+    if (!prevState) return {
+      text: text,
+      original_text_id: selectedTextId,
+    };
+    return {
+      ...prevState,
+      text: text
+    };
+  });
+    let title = textObject?.title || 'title';
+    let author_id = textObject?.author_id || 1;
+    let language = textObject?.language || 'en';
+    db.runAsync(`INSERT INTO texts 
+      (title, author_id, language, text, is_translation, original_text_id) 
+      VALUES (?, ?, ?, ?, ?, ?);`, [title, author_id, language, text, 1, selectedTextId]);
   };
 
-  function handleChange () {
-    console.log('change')
-  }
     return (
       <>
       <ParallaxScrollView
@@ -34,17 +73,19 @@ export default function Write() {
         <ThemedView style={styles.titleContainer}>
           <ThemedText type="title">Write</ThemedText>
         </ThemedView>
-        {texts ? <Pressable onPress={handlePress}>
-          <Text>{selectedText?.original.title}</Text>
-        </Pressable> : null}
-        <TextInput
-        multiline={true}
-        onChangeText={handleChange}>
-        </TextInput>
+            <TextInput
+            value={text}
+            multiline={true}
+            onChangeText={setText}
+            style={styles.textInput}>
+            </TextInput>
+        <Pressable onPress={handlePressSaveButton}>
+          <ThemedText>Save</ThemedText>
+        </Pressable>
       </ParallaxScrollView>
          <Modal visible={dropdownVisible} transparent={true} animationType="fade">
          <View style={styles.modalContainer}>
-           <FlatList
+           {/* <FlatList
              data={texts}
              keyExtractor={(item) => item.id.toString()}
              renderItem={({ item }) => (
@@ -52,7 +93,7 @@ export default function Write() {
                  <ThemedText>{item.original.title}</ThemedText>
                </Pressable>
              )}
-           />
+           /> */}
          </View>
        </Modal>
        </>
@@ -65,6 +106,13 @@ headerImage: {
     bottom: -90,
     left: -35,
     position: 'absolute',
+},
+textInput: {
+    color: 'white',
+    height: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
 },
 titleContainer: {
     flexDirection: 'row',
